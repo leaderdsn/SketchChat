97e6bb5e520fb6b953b03a6077dbb784b2c8c815
leaderdsn <leaderdsn.it@gmail.com>
2022-08-18 20:27:03 +0800

    the main js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-import {sum} from './modules/sum';
+import guestLayoutTemplate from './layouts/guestLayout'
+import mainLayoutTemplate from './layouts/mainLayout'
+import login from './pages/login'
+import signIn from './pages/signin'
+import chat from './pages/chat'
+import error from './pages/error'
+import constructor from './modules/constructor'
+import 'normalize.css'
+import './styles/main.sass'
 
-const root = document.querySelector('#root');
-root.textContent = sum(6, -1).toString();
+const contextGuestLayout = {
+  className: 'guest-layout',
+  content: login, 
+}
+const contextMainLayout = {
+  className: 'main-layout',
+  content: error, 
+}
+
+const root = document.querySelector('#root')
+//root.innerHTML = constructor(contextGuestLayout, guestLayoutTemplate)
+root.innerHTML = constructor(contextMainLayout, mainLayoutTemplate)
+
+openDialog = () => {
+  document.querySelector('#file').click();
+}
+document.querySelector('#upload').addEventListener('click', openDialog);
+
 
