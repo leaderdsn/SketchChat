2e5e456e539e62b65daa49aea773b4aa73de23db
leaderdsn <leaderdsn.it@gmail.com>
2022-08-21 03:02:07 +0800

    avatara render

---
+++ src/components/profile/userAvatar/index.js
@@ -0,0 +1,10 @@
+import userAvatarTemplate from "./userAvatar.tmpl";
+
+const contextUserAvatar = {
+  className: 'y-user-avatar',
+  classNameAvatar: 'y-user-avatar__img',
+  img: '', 
+  userName: 'Иван',
+}
+
+export default window.constructor(contextUserAvatar, userAvatarTemplate);
