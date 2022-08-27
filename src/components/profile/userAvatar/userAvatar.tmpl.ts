d4def07d5278551f10eec0f4cb0e1c6700d5399d
leaderdsn <leaderdsn.it@gmail.com>
2022-08-27 12:45:36 +0800

    typing the user Avatar template

--- src/components/profile/userAvatar/userAvatar.tmpl.ts
+++ src/components/profile/userAvatar/userAvatar.tmpl.ts
@@ -1,4 +1,4 @@
-const userAvatarTemplate = `
+const userAvatarTemplate: string = `
   <div class='y-user-avatar'>
     <div class='y-user-avatar__img'>{{img}}</div>
     <strong class='y-user-avatar__name'>{{userName}}</strong>
