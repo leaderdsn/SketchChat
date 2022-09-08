242a37b868fb2ef5eb55880cd7acdd02acc23cde
leaderdsn <leaderdsn.it@gmail.com>
2022-09-08 22:30:33 +0800

    added types of options

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,14 @@
+import { METHODS } from "~src/utils/HTTPTransport";
+
 export type Nullable<T> = T | null;
 export type Keys<T extends Record<string, unknown>> = keyof T;
 export type Values<T extends Record<string, unknown>> = T[Keys<T>];
+
+export type Options = {
+  method: METHODS;
+  data?: any;
+  timeout?: number;
+  headers?: string;
+};
+
+export type OptionsWithoutMethod = Omit<Options, "method">;
