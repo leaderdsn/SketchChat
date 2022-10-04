20e93a7e670d5c0c2e390a74eebb3b65817bc35b
leaderdsn <leaderdsn.it@gmail.com>
2022-10-04 22:33:49 +0800

    added to utils render, getTime

---
+++ src/utils/render.ts
@@ -0,0 +1,16 @@
+import Block from "~src/utils/block";
+import { Nullable } from "~src/utils/types";
+
+const render = (query: string, block: Nullable<Block>) => {
+  const root: Nullable<Element | string> = document.querySelector(query);
+  if (!root) {
+    throw new Error("Root not found");
+  }
+
+  root.innerHTML = "";
+
+  root.append(block!.getContent());
+  return root;
+};
+
+export default render;
