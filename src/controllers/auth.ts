d7ea2ac59ad926aaec8d7a7306dc059ebd255bec
leaderdsn <leaderdsn.it@gmail.com>
2022-10-04 22:39:44 +0800

    added controllers

---
+++ src/controllers/auth.ts
@@ -0,0 +1,58 @@
+import API, { AuthAPI, SigninData, SignupData } from "~src/api/authAPI";
+import store from "~src/utils/store";
+import Router from "~src/utils/router";
+import MessagesController from "~src/controllers/messages";
+
+class AuthController {
+  private readonly api: AuthAPI;
+
+  constructor() {
+    this.api = API;
+  }
+
+  async signin(data: SigninData) {
+    await this.api.signin(data).then(async (res) => {
+      if (res.status < 400) {
+        await this.fetchUser();
+        Router.go("/chat");
+      } else {
+        alert(res.response.reason);
+      }
+    });
+  }
+
+  async signup(data: SignupData) {
+    await this.api.create(data).then((res) => {
+      if (res.status < 400) {
+        Router.go("/");
+      } else {
+        alert(res.response.reason);
+      }
+    });
+  }
+
+  async fetchUser() {
+    await this.api.read().then((res) => {
+      if (res.status < 400) {
+        store.set("auth", true);
+        store.set("user", res.response);
+      } else {
+        alert(res.response.reason);
+      }
+    });
+  }
+
+  async logout() {
+    await this.api.logout().then((res) => {
+      if (res.status < 400) {
+        MessagesController.closeAll();
+        store.set("auth", false);
+        Router.go("/");
+      } else {
+        alert(res.response.reason);
+      }
+    });
+  }
+}
+
+export default new AuthController();
---
+++ src/controllers/profile.ts
@@ -0,0 +1,62 @@
+import store from "~src/utils/store";
+import API, {
+  ProfileAPI,
+  ProfileData,
+  PasswordData,
+} from "~src/api/profileAPI";
+
+class ProfileController {
+  private readonly api: ProfileAPI;
+
+  constructor() {
+    this.api = API;
+  }
+
+  async changeAvatar(data: FormData) {
+    await this.api.changeAvatar(data)
+      .then((res) => {
+        if (res.status < 400) {
+          console.log('ava', res.response.avatar)
+          store.set("avatar", res.response.avatar);
+          alert("Аватар успешно изменён!");
+        } else {
+          alert(res.response.reason);
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
+
+  async changeProfile(data: ProfileData) {
+    await this.api.changeProfile(data)
+      .then((res) => {
+        if (res.status < 400) {
+          store.set("user", res.response);
+          alert("Профиль успешно изменён!");
+        } else {
+          alert(res.response.reason);
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
+
+  async changePassword(data: PasswordData) {
+    await this.api
+      .changePassword(data)
+      .then((res) => {
+        if (res.status < 400) {
+          alert("Пароль успешно изменён!");
+        } else {
+          alert(res.response.reason);
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
+}
+
+export default new ProfileController();
