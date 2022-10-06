0206d80e575e69a60fd074caef80519f16930963
leaderdsn <leaderdsn.it@gmail.com>
2022-10-06 23:11:17 +0800

    wrapped in a try catch

--- src/utils/WSTransport.ts
+++ src/utils/WSTransport.ts
@@ -66,13 +66,18 @@ export default class WSTransport extends EventBus {
     });
 
     socket.addEventListener("message", (message) => {
-      const data = JSON.parse(message.data);
+      try {
+        const data = JSON.parse(message.data);
 
-      if (data.type && data.type === "pong") {
-        return;
+        if (data.type && data.type === "pong") {
+          return;
+        }
+
+        this.emit(WSTransportEvents.Message, data);
+      } catch (e) {
+        throw new Error(e as string)
       }
-
-      this.emit(WSTransportEvents.Message, data);
+      
     });
   }
 }
